cb43b1fbed66108a731759f5bea38e883f8ec134
Saahil Shangle <saahilshangle@gmail.com>
2020-06-16 23:27:17 -0700

    Check of basic port setup and proxy functionality

--- server/server.js
+++ server/server.js
@@ -5,6 +5,11 @@ const port = process.env.PORT || 5000;
 // console.log that your server is up and running
 app.listen(port, () => console.log('Listening on port ${port}'));
 
+// check server functionality
+app.get('/', (req, res) => {
+    res.send('Test')
+});
+
 // create a GET route
 app.get('/express_backend', (req, res) => {
     res.send({express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'})
