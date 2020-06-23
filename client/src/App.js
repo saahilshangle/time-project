dcadaf819f0017550537ae86aadb97153088f4e8
Saahil Shangle <saahilshangle@gmail.com>
2020-06-22 22:16:33 -0700

    Changed component style and tested state variable declaration for #1.

--- client/src/App.js
+++ client/src/App.js
@@ -1,63 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 //import app from '../server/server';
 
-function App() {
-
-  function constructor(props) {
+class App extends React.Component {
+  
+  constructor(props) {
     super(props);
-    this.state = { apiResponse: "" };
+    this.state = { apiResponse: "First" };
   }
 
-  function callAPI() {
+  callAPI() {
     fetch("http://localhost:5000/express_backend")
         .then(res => res.text())
         .then(res => this.setState({ apiResponse: res }));
   }
 
-  function componentWillMount() {
+  componentWillMount() {
     this.callAPI();
   }
 
-  function myFunction() {
+  //componentWillMount();
+
+  myFunction() {
     let response = fetch("http://localhost:5000/express_backend");
     return response;
   }
 
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React.
+          </a>
+        </header>
+        <body>
+          <p>Blogging Platform App</p>
+          <a
+            href="https://raddevon.com/articles/10-great-web-development-learning-project-ideas/"
+            className="App-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn more
+          </a><br></br>
+          {/* <button onclick={myFunction}>Create new post</button> */}
+          <a href="http://localhost:5000/">This is the backend</a>
+          <p>{this.state.apiResponse}</p>
+        </body>
+      </div>
+    );
+  }
   
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React.
-        </a>
-      </header>
-      <body>
-        <p>Blogging Platform App</p>
-        <a
-          href="https://raddevon.com/articles/10-great-web-development-learning-project-ideas/"
-          className="App-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn more
-        </a><br></br>
-        <button onclick={myFunction}>Create new post</button>
-        <a href="http://localhost:5000/">This is the backend</a>
-        <p className="App-intro">{this.state.apiResponse}</p>
-      </body>
-    </div>
-  );
 }
 
 export default App;
