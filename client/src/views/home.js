dd005fc09e7eb44f1576dc40b4297cd11601178f
Saahil Shangle <saahilshangle@gmail.com>
2020-09-02 14:09:11 -0700

    Rearranged existing content, boilerplate for profile creation

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -3,59 +3,7 @@ import './../App.css';
 
 export default class Home extends React.Component {
     
-    constructor(props) {
-        super(props);
-        this.state = { 
-            apiResponse: "First", 
-            mongoResponse: "Second",
-            textBox: "not entered yet",
-            textResponse: "[not entered or server not running]"
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({textBox: event.target.value});
-    }
-
-    handleSubmit(event) {
-        //alert(this.state.textBox);
-        event.preventDefault();
-        this.callTextBox(this.state.textBox);
-    }
-
-    // eslint-disable-next-line
-    callAPI() {
-        fetch("http://localhost:5000/express_backend")
-            .then(res => res.text())
-            .then(res => this.setState({ apiResponse: res }));
-    }
-
-    callDatabase() {
-        fetch("http://localhost:5000/mongo_backend")
-            .then(res => res.text())
-            .then(res => this.setState({ mongoResponse: res }));
-    }
-
-    callTextBox(content) {
-        fetch("http://localhost:5000/text_input?name=" + content)
-            .then(res => res.text())
-            .then(res => this.setState({ textResponse: res }))
-    }
-
-    componentWillMount() {
-        this.callAPI();
-        this.callDatabase();
-        //this.myFunction();
-    }
-
-    myFunction() {
-        // let response = fetch("http://localhost:5000/express_backend");
-        // return response;
-        console.log("testing12345");
-        // This has more info on event listeners KEEP THIS
-        // https://reactjs.org/docs/forms.html
+    componentDidMount() {
     }
 
     render() {
@@ -63,20 +11,8 @@ export default class Home extends React.Component {
             <body className="row">
                 <div className="column left" />
                 <div className="column middle">
-                    {/* <p>{this.state.apiResponse}</p> */}
-                    {/* <p>{this.state.mongoResponse}</p> */}
                     <br></br>
-                    <div className="h-content">
-                        <p>Sell your time! College application review, tutoring, lessons, consultations, and more!</p>
-                        <form onSubmit={this.handleSubmit}>
-                            <label>
-                                Name: <space />
-                                <input type="text" value={this.state.value} onChange={this.handleChange}/>
-                            </label>
-                            <input type="submit" value="Submit" />
-                        </form>
-                        <p>Your name is: {this.state.textResponse}</p>
-                    </div>
+                    <p className="temp">Sell your time! College application review, tutoring, lessons, consultations, and more!</p>
                 </div>
                 <div className="column right" />
             </body>
